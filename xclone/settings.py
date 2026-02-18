6ab69fd3a101f1f582d381b5d25e2f54c84be7bb
Mandisa Khumalo <A0070215@wits.ac.za>
2026-02-18 19:44:26 +0200

    updates

--- xclone/settings.py
+++ xclone/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'x',
     'cloudinary',
     'cloudinary_storage',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'xclone.urls'
